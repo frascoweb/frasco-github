63a27ad4461a1a1c5740639a7f37e8dff1b3c7e0
maximebf <maxime.bouroumeau@gmail.com>
2015-08-11 17:48:17 +0200

    refactored to module better support for scopes

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
     name='frasco-github',
-    version='0.2',
+    version='0.3',
     url='http://github.com/frascoweb/frasco-github',
     license='MIT',
     author='Maxime Bouroumeau-Fuseau',
     author_email='maxime.bouroumeau@gmail.com',
     description="Github integration for Frasco",
-    packages=find_packages(),
+    py_modules=['frasco_github'],
     zip_safe=False,
     platforms='any',
     install_requires=[
